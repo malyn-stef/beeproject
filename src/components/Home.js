2c05df83630e1cfad23490fa345b2b254fdf94ef
+ <+>
2023-05-07 12:52:56 -0600

    added home

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function Home() {
+  return (
+    <div>
+      <h1 className="welcome">Welcome to the BusyBee!</h1>
+      <img className='flowergif' src={'https://i.pinimg.com/originals/dd/bf/00/ddbf00d0dbaea8814cca8cc19796fd8e.gif'} alt='flower' />
+      <img className="welcome" src={'https://media0.giphy.com/media/U6YxrKZ84AfppW48r4/giphy.gif'} alt="flyingbee" />
+
+    </div>
+  )
+}
+
+export default Home
